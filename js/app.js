dbcc52b9e8ea936b5de27a1a25c69c0cf45f7aad
mohammadsilwadi <mohammadsilwadi1@gmail.com>
2021-06-15 17:56:46 +0300

    added some css

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 /* eslint-disable no-undef */
 
 'use strict';
-let parentElement = document.getElementById('salesData');
+
 let operatingHours=['6 am', '7 am', '8 am', '9 am', '10 am', '11 am', '12 pm', '1 pm', '2 pm', '3 pm', '4 pm', '5 pm', '6 pm', '7 pm', '8 pm'];
 let locationDetailsArray=[];
 function LocationDetails(locationName, minCustomersHourly, avgPerCustHourly, maxCustomersHourly, cookiesSalesHourly, randomCustomersHourly, totalSalesDaily){
